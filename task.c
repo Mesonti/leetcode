facee6e5b711238aa626f5d2a6e7f800821faff3
Ivan Samyshev <samyshev.2000@mail.ru>
2023-03-23 09:04:33 +0400

    init

---
+++ task.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int *twoSum(int *nums, int numsSize, int target, int *returnSize)
+{
+    for (int i = 0; i < target; ++i)
+    {
+        
+    }
+    
+}
+
+int main()
+{
+
+}
