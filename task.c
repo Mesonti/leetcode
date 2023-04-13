66c88c74579428ea09b0a12c4d44c5b1eb4b64f5
Ivan Samyshev <samyshev.2000@mail.ru>
2023-04-13 23:36:52 +0400

    add .kt for tasks

--- task.c
+++ task.c
@@ -4,14 +4,27 @@
 
 int *twoSum(int *nums, int numsSize, int target, int *returnSize)
 {
-    for (int i = 0; i < target; ++i)
+    for (int i = 0; i < nums[i]; ++i)
     {
-        
+        numsSize = nums[i] + nums[i];
+        if (numsSize == target)
+        {
+            return returnSize;
+            printf("%d", returnSize);
+        }
     }
-    
 }
 
 int main()
 {
-
+    int nums[] = {2, 7, 11, 15};
+    int numsSize = 0;
+    int target = 9;
+    int returnSize[] = {};
+    
+    for (int i = 0; i < nums[i]; i++)
+    {
+        returnSize[i] += nums[i + 1];
+        printf("%d\n", returnSize[i]);
+    }
 }
