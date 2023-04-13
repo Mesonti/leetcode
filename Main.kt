426fc26464c039ddbfc26dfbfa67f50d60946c78
Ivan Samyshev <samyshev.2000@mail.ru>
2023-04-14 00:06:08 +0400

    start 1480 task

--- Main.kt
+++ Main.kt
@@ -1,5 +1,22 @@
 fun main() {
 
-    
+    val arr: IntArray = intArrayOf(1, 3, 3, 6)
+    println(runningSum(arr))
 
 }
+
+
+fun runningSum(nums: IntArray): IntArray {
+    var result = 0
+    for (item in nums) {
+        result += item
+        // сделать добавление в массив, связано с индексами
+    }
+    return nums
+
+}
+
+/*
+Input: nums = [1,2,3,4]
+Output: [1,3,6,10]
+Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].*/
